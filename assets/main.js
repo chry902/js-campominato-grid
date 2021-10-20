780e72fe42e5825e7414d82ea35ce2f89dff82df
chry902 <tchry902@gmail.com>
2021-10-20 15:26:20 +0200

    struttura griglia

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,49 @@
-console.log(`ciao`);
+//selezionare un elemento dalla DOM continer
+
+const container = document.querySelector(`.container`)
+
+
+//genera una griglia da 64 elementi con un ciclo
+for (let i = 0; i < 100; i++) {
+
+
+    //crea un elemento DOM da menttere nella grilia (cella)
+    const divEl = document.createElement(`div`);
+
+
+    //aggiungi una classe all'elemento appena creato
+    divEl.className = `grid_cell`;
+
+
+    //appendi l'elemento creato prima al container 
+    container.append(divEl);
+
+    //attacca un event listner a ciascun elemento creato nel ciclo
+    divEl.addEventListener(`click`, function () {
+
+        this.StyleSheet.backgroundColor = `greenyellow`
+
+
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
