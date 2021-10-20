3625052e294458c1186369340d68d295db8af7ce
chry902 <tchry902@gmail.com>
2021-10-20 17:53:41 +0200

    creazione input e buttom

--- assets/main.js
+++ assets/main.js
@@ -1,44 +1,59 @@
 //crea un input per selezionare la difficolta 
-let utente = prompt(`selziona la difficoltà `);
+let livello = prompt(`selziona la difficoltà `);
 
-let difficolta1 = "";
-let difficolta2 = "";
-let difficolta3 = "";
-
-//selezionare un elemento dalla DOM continer
-
-const container = document.querySelector(`.container`)
-
-
-//genera una griglia con un ciclo
-for (let i = 0; i < 100; i++) {
-    //let element = `<divEL class= "grid_cell grid_cell--${i}"> ${i}</divEL>`;
-    //  section.insertAdjacentHTML("beforeend", element);
-
-
-
-    //crea un elemento DOM da menttere nella grilia (cella)
-    const divEl = document.createElement(`div`);
-
-
-
-    //aggiungi una classe all'elemento appena creato
-    divEl.className = `grid_cell`;
-    divEl.innerHTML = `${i}`;
-
-    //appendi l'elemento creato prima al container 
-    container.append(divEl);
-
-    //attacca un event listner a ciascun elemento creato nel ciclo
-    divEl.addEventListener(`click`, function () {
-
-        this.Style.backgroundColor = `green`;
-
-
-    })
+const livello = document.getElementById(`livello`);
+const esegui = document.getElementById(`esegui`);
+esegui.addEventListener(`click`, function){
 
 }
+generaGriglia(livello)
 
+//genera una griglia con un ciclo
+function generaGriglia(livello) {
+    let numeroCelle;
+
+    if (livello == 1) {
+        numeroCelle = 100;
+    } else if (livello == 2) {
+        numeroCelle = 81;
+    } else {
+        numeroCelle = 49;
+    }
+
+    console.log(numeroCelle)
+
+    //selezionare un elemento dalla DOM continer
+
+    const container = document.querySelector(`.container`)
+
+    for (let i = 0; i < numeroCelle; i++) {
+        //let element = `<divEL class= "grid_cell grid_cell--${i}"> ${i}</divEL>`;
+        //  section.insertAdjacentHTML("beforeend", element);
+
+
+
+        //crea un elemento DOM da menttere nella grilia (cella)
+        const divEl = document.createElement(`div`);
+
+
+
+        //aggiungi una classe all'elemento appena creato
+        divEl.className = `grid_cell`;
+        divEl.innerHTML = `${i}`;
+
+        //appendi l'elemento creato prima al container 
+        container.append(divEl);
+
+        //attacca un event listner a ciascun elemento creato nel ciclo
+        divEl.addEventListener(`click`, function () {
+
+            this.style.backgroundColor = `green`;
+
+
+        })
+
+    }
+}
 
 
 
