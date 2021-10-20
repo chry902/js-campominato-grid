a1c5674e35a81ef948e2fc6d7b175a84469619f5
chry902 <tchry902@gmail.com>
2021-10-20 16:45:26 +0200

    creando le varie difficolta

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,17 @@
-
-let utente = "";
 //crea un input per selezionare la difficolta 
+let utente = prompt(`selziona la difficoltà `);
 
+let difficolta1 = "";
+let difficolta2 = "";
+let difficolta3 = "";
 
 //selezionare un elemento dalla DOM continer
 
 const container = document.querySelector(`.container`)
 
 
-//genera una griglia da 64 elementi con un ciclo
-for (let i = 0; i <= 100; i++) {
+//genera una griglia con un ciclo
+for (let i = 0; i < 100; i++) {
     //let element = `<divEL class= "grid_cell grid_cell--${i}"> ${i}</divEL>`;
     //  section.insertAdjacentHTML("beforeend", element);
 
@@ -30,7 +32,7 @@ for (let i = 0; i <= 100; i++) {
     //attacca un event listner a ciascun elemento creato nel ciclo
     divEl.addEventListener(`click`, function () {
 
-        this.StyleSheet.backgroundColor = `green`;
+        this.Style.backgroundColor = `green`;
 
 
     })
