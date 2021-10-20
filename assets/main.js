c02e60750cb90fc286964495e3ed969f806f7474
chry902 <tchry902@gmail.com>
2021-10-20 15:50:37 +0200

    creazione celle

--- assets/main.js
+++ assets/main.js
@@ -12,8 +12,8 @@ for (let i = 0; i < 100; i++) {
     //crea un elemento DOM da menttere nella grilia (cella)
     let divEl = document.createElement(`div`);
 
-    let element = `<divEL class= "grid_cell grid_cell--${i}"> ${i}</divEL>`;
-    section.insertAdjacentHTML("beforeend", element);
+    //let element = `<divEL class= "grid_cell grid_cell--${i}"> ${i}</divEL>`;
+    //section.insertAdjacentHTML("beforeend", element);
 
     //aggiungi una classe all'elemento appena creato
     divEl.className = `grid_cell`;
