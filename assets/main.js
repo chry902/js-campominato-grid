a9dede440343fc42e12e92c4a53f3ac2b24ccefc
chry902 <tchry902@gmail.com>
2021-10-20 16:14:25 +0200

    avanzamento

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,28 @@
+
+let utente = "";
+//crea un input per selezionare la difficolta 
+
+
 //selezionare un elemento dalla DOM continer
 
 const container = document.querySelector(`.container`)
 
 
 //genera una griglia da 64 elementi con un ciclo
-for (let i = 0; i < 100; i++) {
-
+for (let i = 0; i <= 100; i++) {
+    //let element = `<divEL class= "grid_cell grid_cell--${i}"> ${i}</divEL>`;
+    //  section.insertAdjacentHTML("beforeend", element);
 
 
 
     //crea un elemento DOM da menttere nella grilia (cella)
-    let divEl = document.createElement(`div`);
+    const divEl = document.createElement(`div`);
 
-    //let element = `<divEL class= "grid_cell grid_cell--${i}"> ${i}</divEL>`;
-    //section.insertAdjacentHTML("beforeend", element);
+
 
     //aggiungi una classe all'elemento appena creato
     divEl.className = `grid_cell`;
-
+    divEl.innerHTML = `${i}`;
 
     //appendi l'elemento creato prima al container 
     container.append(divEl);
@@ -25,7 +30,7 @@ for (let i = 0; i < 100; i++) {
     //attacca un event listner a ciascun elemento creato nel ciclo
     divEl.addEventListener(`click`, function () {
 
-        this.StyleSheet.backgroundColor = `greenyellow`
+        this.StyleSheet.backgroundColor = `green`;
 
 
     })
